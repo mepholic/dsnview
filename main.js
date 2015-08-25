edf424a506e171cce5c38d1cdacad8471549b841
mepholic <gikid0@gmail.com>
2015-08-25 06:44:46 +0000

    something is starting to work

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
-var request = require('./request.js');
+var inspect = require('eyes').inspector({
+    maxLength: false
+});
 
-request('https://eyes.nasa.gov/dsn/config.xml');
+var fetchLive = require('./lib/fetchLive.js');
+var parseXML = require('xml2js').parseString;
+
+fetchLive.config(function(xml) {
+    parseXML(xml, function(err, result) {
+        // Enumerate spacecraft and shove them into an object
+        var spacecraft = {};
+        var scList = result.config.spacecraftMap[0].spacecraft;
+        for (scId in scList) {
+            var scRoot = scList[scId]
+            var scObject = scRoot.$;
+            var scName = scObject.name;
+            spacecraft[scName] = scObject;
+            delete spacecraft[scName].name;
+        };
+        // Enumerate sites and shove them into an object
+        var site = {};
+        var dish = {};
+        var siteList = result.config.sites[0].site;
+        for (siteId in siteList) {
+            var siteRoot = siteList[siteId];
+            var siteObject = siteRoot.$;
+            var siteName = siteObject.name;
+            site[siteName] = siteObject;
+            delete site[siteName].name;
+
+            // Enumerate dishes and shove them into an object
+            var dishList = siteRoot.dish;
+            for (dishId in dishList) {
+                var dishRoot = dishList[dishId];
+                var dishObject = dishRoot.$;
+                var dishName = dishObject.name;
+                dish[dishName] = dishObject;
+                dish[dishName]['site'] = siteName;
+                delete dish[dishName].name;
+            };
+        };
+        console.log(dish);
+    });
+});
